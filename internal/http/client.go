202d9d56219fdaff82f0053d678a4a507884624a
Konstantinos Kasidiaris <kkasidiaris@gmail.com>
2026-02-15 20:21:23 +0200

    chore: tidy up tests and increase coverage

--- internal/http/client.go
+++ internal/http/client.go
@@ -8,21 +8,27 @@ import (
 	"github.com/konkasidiaris/gitvault/internal/config"
 )
 
-var UserAgent = fmt.Sprintf("GitVault/%s", config.GetGitVaultVersion())
-
 type Client struct {
-	Client *http.Client
+	Client    *http.Client
+	versionFn func() string
 }
 
 func NewClient() *Client {
+	return newClientWithVersion(config.GetGitVaultVersion)
+}
+
+func newClientWithVersion(versionFn func() string) *Client {
+
 	return &Client{
 		Client: &http.Client{
 			Timeout: 30 * time.Second,
 		},
+		versionFn: versionFn,
 	}
 }
 
 func (c *Client) Do(req *http.Request) (*http.Response, error) {
+	UserAgent := fmt.Sprintf("GitVault/%s", c.versionFn())
 	req.Header.Set("User-Agent", UserAgent)
 	return c.Client.Do(req)
 }
